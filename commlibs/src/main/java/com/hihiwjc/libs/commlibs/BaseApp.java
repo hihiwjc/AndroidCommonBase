7aa7265827d2724b2ee44b30d381d7421dbf4bef
hihiwjc <hihiwjc@live.com>
2016-06-17 20:02:19 +0800

    1.add Log formatter

---
+++ commlibs/src/main/java/com/hihiwjc/libs/commlibs/BaseApp.java
@@ -0,0 +1,14 @@
+package com.hihiwjc.libs.commlibs;
+
+import android.app.Application;
+
+/**
+ * <br/>Author:hihiwjc
+ * <br/>Email:hihiwjc@live.com
+ * <br/>Date:2016/6/17 0017
+ * <br/>Func:Application基类
+ */
+
+public class BaseApp extends Application {
+
+}
---
+++ commlibs/src/main/java/com/hihiwjc/libs/commlibs/utils/logger/LogLevel.java
@@ -0,0 +1,14 @@
+package com.hihiwjc.libs.commlibs.utils.logger;
+
+public enum LogLevel {
+
+    /**
+     * Prints all logs
+     */
+    FULL,
+
+    /**
+     * No log will be printed
+     */
+    NONE
+}
