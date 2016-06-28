1aa3107ed8006e0c2c022abe1e0a3b11a3c015ff
hihiwjc <hihiwjc@live.com>
2016-06-28 16:59:36 +0800

    1.alter the AutoScrollViewPager speed

--- commlibs/src/main/java/cn/hihiwjc/libs/commlibs/views/autoscrollpager/CustomDurationScroller.java
+++ commlibs/src/main/java/cn/hihiwjc/libs/commlibs/views/autoscrollpager/CustomDurationScroller.java
@@ -17,29 +17,14 @@ import android.widget.Scroller;
 public class CustomDurationScroller extends Scroller {
 
     private double scrollFactor = 1;
-
-    public CustomDurationScroller(Context context) {
-        super(context);
-    }
+    private int mDuration;
 
     public CustomDurationScroller(Context context, Interpolator interpolator) {
         super(context, interpolator);
     }
 
     /**
-     * not exist in android 2.3
-     *
-     * @param context
-     * @param interpolator
-     * @param flywheel
-     */
-    // @SuppressLint("NewApi")
-    // public CustomDurationScroller(Context context, Interpolator interpolator, boolean flywheel){
-    // super(context, interpolator, flywheel);
-    // }
-
-    /**
-     * Set the factor by which the duration will change
+     * Set the factor by which the mDuration will change
      */
     public void setScrollDurationFactor(double scrollFactor) {
         this.scrollFactor = scrollFactor;
@@ -47,6 +32,7 @@ public class CustomDurationScroller extends Scroller {
 
     @Override
     public void startScroll(int startX, int startY, int dx, int dy, int duration) {
-        super.startScroll(startX, startY, dx, dy, (int)(duration * scrollFactor));
+        this.mDuration = 1600;
+        super.startScroll(startX, startY, dx, dy, this.mDuration/*(int)(mDuration * scrollFactor)*/);
     }
 }
